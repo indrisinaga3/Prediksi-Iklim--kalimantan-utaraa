b5a0b4b6e669c204c8bddab75712eb3a953ece76
indri sinaga <indrisinaga211@gmail.com>
2025-11-26 12:19:29 +0700

    Update KALUT.py

--- KALUT.py
+++ KALUT.py
@@ -139,7 +139,6 @@ csv = future.to_csv(index=False).encode("utf8")
 st.download_button(
     "📥 Download Dataset Prediksi",
     data=csv,
-    file_name="DATA KALUT 2015-2025.csv",
+    file_name="prediksi_jawa_timur.csv",
     mime="text/csv"
 )
-
